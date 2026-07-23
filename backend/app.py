6985e047262eda7262b7e682ee83cdb268e219a1
25f2006063 <25f2006063@study.iitm.ac.in>
2026-07-23 14:41:33 +0530

    Milestone-0: Initial project setup

---
+++ backend/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+from config import Config
+from extensions import db, bcrypt, jwt
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    CORS(app)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    @app.route("/")
+    def home():
+        return jsonify({
+            "message": "Trekking Management API Running"
+        })
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
